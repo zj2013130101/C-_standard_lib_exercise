7073176300592ca1522c1527c9ba0f32def797a9
Joan <joandouble11@gmail.com>
2019-07-17 15:05:52 +0800

    create c++ standart library exercise

---
+++ cmake-build-debug/iterator/CMakeFiles/iterator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iterator.dir/main.cpp.o"
+  "iterator.pdb"
+  "iterator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iterator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
